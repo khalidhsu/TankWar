5a1de7db2e5122845dc7bf9e9a116fa7b19c787a
Khalid Hsu <khalid@khalid-K42JE.(none)>
2013-05-14 10:01:18 +0800

    2013年 05月 14日 星期二 10:01:18 HKT

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ TankWar
 =======
 
 Tank War
+
+[check it out](http://khalidhsu.github.io/posts/2013/May/14/tank-teach-01/)
+
---
+++ tankwar-tech-01-1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python  
+#-*-coding:utf-8-*-
+import pygame
+# Initialize the game engine
+pygame.init()    #pygame初始化
+size=[700,500]    #指定窗口尺寸
+screen=pygame.display.set_mode(size)    #新建一个窗口
+pygame.display.set_caption("坦克大战")    #设置窗口标题
+done=False
+while done==False:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            done=True 
+    pygame.display.flip()#刷新屏幕
+pygame.quit ()
